04e91fa0cc3696bf87eb50c95547d5095ee53538
Sarah Pai <pai.sarah89@gmail.com>
2018-08-26 20:14:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // add solution here
+
+// for loop
+
+function theBeatlesPlay(musicians, instruments) {
+  // musicians, instruments are both arrays
+  var newArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArray;
+}
+
+
+
+// while loop
+
+function johnLennonFacts(facts){
+  var newFacts = [];
+  var i = 0
+  while ( i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+return newFacts;
+}
+  
+  
+  
+  // do while loop
+function iLoveTheBeatles(n){
+  var newNum = [];
+  var i = 0;
+  do {
+  newNum = "I love the Beatles!"
+  i++;
+   } while (i < 17);
+return newNum;
+}
